12786a76b92b8c1cb2476c4b2b61a4aa6ee439ed
Zach Keskinen <zachkeskinen@gmail.com>
2023-02-17 09:50:40 -0700

    non absolute pickle filepath to appease git

--- tests/test_wetsnow.py
+++ tests/test_wetsnow.py
@@ -16,7 +16,7 @@ class TestWetSnowMask(unittest.TestCase):
     Test functionality of identify and setting wet snow paper from Lievens et al 2021
     """
 
-    with open('/Users/zachkeskinen/Documents/spicy-snow/tests/test_data/2_img_ds', 'rb') as f:
+    with open('./tests/test_data/2_img_ds', 'rb') as f:
         ds = pickle.load(f)
 
     def test_id_newly_wet(self, ds = ds):
