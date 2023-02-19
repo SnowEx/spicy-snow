410e0ce76aaa550fdc512729e4de470a87417214
Zach Keskinen <zachkeskinen@gmail.com>
2023-02-19 16:03:58 -0700

    test for downloads initial import clear

--- tests/test_downloads.py
+++ tests/test_downloads.py
@@ -9,7 +9,7 @@ import pandas.testing as pd_testing
 import sys
 from os.path import expanduser
 sys.path.append(expanduser('.'))
-from spicy_snow.download.sentinel1 import s1_img_search, download_s1_imgs
+from spicy_snow.download.sentinel1 import s1_img_search
 from spicy_snow.download.forest_cover import download_fcf, add_fcf
 
 class TestSentinel1Search(unittest.TestCase):
