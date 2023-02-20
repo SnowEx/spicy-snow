c883462b96df29ca679d9995a883d5b197cf5a82
Zach Keskinen <zachkeskinen@gmail.com>
2023-02-20 11:28:36 -0700

    refactored forest_cover download to match snow_cover and range 0-1

--- spicy_snow/download/forest_cover.py
+++ spicy_snow/download/forest_cover.py
@@ -13,15 +13,16 @@ import xarray as xr
 sys.path.append(expanduser('~/Documents/spicy-snow'))
 from spicy_snow.utils.download import url_download
 
-def download_fcf(out_fp: str) -> xr.DataArray:
+def download_fcf(dataset: xr.Dataset, out_fp: str) -> xr.DataArray:
     """
     Download PROBA-V forest-cover-fraction images.
 
     Args:
+    dataset: large dataset to add IMS data to
     out_fp: filepath to save tiff of results
 
     Returns:
-    None
+    dataset: large dataset with 'fcf' added as data variable
     """
     # this is the url from Lievens et al. 2021 paper
     fcf_url = 'https://zenodo.org/record/3939050/files/PROBAV_LC100_global_v3.0.1_2019-nrt_Tree-CoverFraction-layer_EPSG-4326.tif'
@@ -29,25 +30,19 @@ def download_fcf(out_fp: str) -> xr.DataArray:
     url_download(fcf_url, out_fp)
     # open as dataArray and return
     fcf = rxa.open_rasterio(out_fp)
-    return fcf
 
-def add_fcf(dataset: xr.Dataset, fcf: xr.DataArray) -> xr.Dataset:
-    """
-    Add xarray dataArray of forest cover data to a larger xarray Dataset.
-
-    Args:
-    dataset: large dataset to add IMS data to
-    fcf: fcf dataArray for all data
-
-    Returns:
-    dataset: large dataset with 'fcf' added as data variable
-    """
     # clip FCF to dataset boundaries (from user defined geometry)
     fcf = fcf.rio.clip_box(*dataset['s1'].rio.bounds())
     # reproject FCF to match dataset
     fcf = fcf.rio.reproject_match(dataset['s1'])
     # remove band dimension as it only has one band
     fcf = fcf.squeeze('band')
+    # if max is greater than 1 set to 0-1
+    if fcf.max() >= 1:
+        fcf = fcf / 100
+    
+    assert fcf.max() <= 1, "Forest cover fraction must be bounded 0-1"
+    assert fcf.min() >= 0, "Forest cover fraction must be bounded 0-1"
     # merge FCF and name it 'fcf' as a data variable
     dataset = xr.merge([dataset, fcf.rename('fcf')])
 
