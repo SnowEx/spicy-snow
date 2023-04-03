de7bc34f1f32ffa1c6150299ffc9c2d2d1444d13
Zach Keskinen <Zachkeskinen@gmail.com>
2023-04-03 09:58:32 -0600

    lowman compare script

--- scripts/lowman_compare.py
+++ scripts/lowman_compare.py
@@ -7,6 +7,8 @@ import pandas as pd
 import xarray as xr
 import rioxarray as rxa
 
+from pathlib import Path
+
 # Add main repo to path
 import sys
 from os.path import expanduser
@@ -17,13 +19,13 @@ from spicy_snow.IO.user_dates import get_input_dates
 
 area = shapely.geometry.box(-117, 43, -113, 46)
 
-os.makedirs('/home/zacharykeskinen/scratch/spicy-lowman/', exist_ok = True)
-out_nc = f'/home/zacharykeskinen/scratch/spicy-lowman/spicy-lowman.nc'
+os.makedirs(Path('~/scratch/spicy-lowman/').expanduser(), exist_ok = True)
+out_nc = Path('~/scratch/spicy-lowman/spicy-lowman.nc').expanduser()
 
 dates = get_input_dates('2021-04-01')
 
 spicy_ds = retrieve_snow_depth(area = area, dates = dates, 
-                               work_dir = '/home/zacharykeskinen/scratch/data/', 
+                               work_dir = Path('~/scratch/spicy-lowman/data/').expanduser(), 
                                job_name = f'spicy-lowman-v1', 
                                existing_job_name = 'spicy-lowman-v1',
                                debug=False)
